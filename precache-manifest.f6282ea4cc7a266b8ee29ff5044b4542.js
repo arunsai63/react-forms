8280ca8d36284315d603b24909650ff3e1b11419
arun <arun@github.com>
2025-03-30 01:56:44 +0530

    added build files

---
+++ precache-manifest.f6282ea4cc7a266b8ee29ff5044b4542.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e13c4a9b3096965b2f7d598c105f793",
+    "url": "/react-forms/index.html"
+  },
+  {
+    "revision": "56402d1ccdaad601c51e",
+    "url": "/react-forms/static/js/2.9a9a9da8.chunk.js"
+  },
+  {
+    "revision": "9a6f6e3c097387e3ec42",
+    "url": "/react-forms/static/js/main.e1a8315d.chunk.js"
+  },
+  {
+    "revision": "ae477fb449b1a1b38199",
+    "url": "/react-forms/static/js/runtime~main.82238d43.js"
+  }
+]);
